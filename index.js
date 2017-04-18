cfbe604e8fb893a6620e2421b092a099d2181297
Bevan Rudge <bevan@js.geek.nz>
2017-04-18 20:07:22 +1200

    Get each notice and parse out useful data

--- index.js
+++ index.js
@@ -4,9 +4,34 @@ const request = require('request')
 const baseUrl = 'http://www.aucklandcouncil.govt.nz'
 
 function main() {
-    getNotices().then((notices) => {
-        console.log(notices.Other[0].url)
-        process.exit()
+    getNotices()
+        .then(notices => {
+            return Promise.all(notices.Other.map(getNotice))
+        })
+        .then((notices) => {
+            console.log(notices)
+            process.exit()
+        })
+}
+
+function getNotice({url, text}) {
+    return new Promise(resolve => {
+        request(url, (error, response, html) => {
+            if (error) throw error
+            const $ = cheerio.load(html)
+            const content = $('#content_middle')
+            const notice = {
+                title: content.find('h2').text().trim(),
+                date: content.find('.date').text().trim(),
+                content: content.find('.pagecontent > div').html(),
+                unknown: {
+                    full: content.find('.full').text().trim(),
+                    byline: content.find('.byline').text().trim(),
+                }
+            }
+
+            resolve(notice)
+        })
     })
 }
 
@@ -14,13 +39,12 @@ function getNotices() {
     return new Promise(resolve => {
         const url = `${baseUrl}/EN/newseventsculture/OurAuckland/PublicNotices/Pages/Home.aspx`
 
-
         request(url, (error, response, html) => {
             if (error) throw error
-
             const $ = cheerio.load(html)
+
             elements = []
-            $('#ZoneA').find('a').filter(function() {
+            $('#ZoneA').find('a').filter(function () {
                 elements.push($(this))
             })
 
