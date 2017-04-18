eebe88c069ba49fff0af6980bb0cc573aaf3f4c5
Bevan Rudge <bevan@js.geek.nz>
2017-04-18 19:11:56 +1200

    Trying to get cheerio to work

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 const cheerio = require('cheerio')
 const request = require('request')
 
-url = 'http://www.imdb.com/title/tt1229340/';
+url = 'http://www.aucklandcouncil.govt.nz/EN/newseventsculture/OurAuckland/PublicNotices/Pages/Home.aspx';
 
 request(url, function(error, response, html){
-    if(!error){
-        var $ = cheerio.load(html);
+    if (error) throw error
 
-        var title, release, rating;
-        var json = { title : "", release : "", rating : ""};
+    const $ = cheerio.load(html)
+    const results = []
+    const selectors = [
+        'a[href]',
+        // '*:has(> a:not([href]))',
+    ]
 
-        // We'll use the unique header class as a starting point.
-
-        $('.title_wrapper').filter(function(){
-
-            // Let's store the data we filter into a variable so we can easily see what's going on.
-
-            var data = $(this);
-
-            // In examining the DOM we notice that the title rests within the first child element of the header tag.
-            // Utilizing jQuery we can easily navigate and get the text by writing the following code:
-
-            title = data.children().first().text();
-
-            // Once we have our title, we'll store it to the our json object.
-
-            json.title = title;
-        })
-        console.log(json)
-        process.exit()
-    }
+    $('#ZoneA').find(selectors.join(',')).filter(() => {
+        const element = $(this)
+        const text = element.text().trim()
+        if (text) console.log(text)
+    })
+    // console.log(results)
+    process.exit()
 })
